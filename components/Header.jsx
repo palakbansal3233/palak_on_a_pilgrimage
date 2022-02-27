084a7666d3eba728d9ff20b26c35f1be4d550fb3
palakbansal3233 <palakbansal3233@gmail.com>
2022-02-27 21:26:12 +0530

    Background image

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Palak_on_a_Pilgrimage</span>
+            <span className="cursor-pointer font-bold text-2xl text-white">Palak_on_a_Pilgrimage</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
